9cff2986e3c6ec55b15c605e79fd4584f1814100
RoshanKhatri0 <lastkhatri@gmail.com>
2024-01-09 19:58:31 +0545

    first commit

---
+++ controllers/beauticianProfileController.js
@@ -0,0 +1,7 @@
+const B_Profile =require('../models/beauticianProfileModel')
+
+exports.postB_Profile=async(req,res)=>{
+    let b_profile = new B_Profile({
+        
+    })
+}
---
+++ models/beauticianProfileModel.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose')
+
+const contactInfoSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        trim: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+const socialsSchema = new mongoose.Schema({
+    socials_instragram:{
+        type: String,
+        trim: true
+    },
+    socials_facebook:{
+        type: String,
+        trim: true
+    },
+    socials_tiktok:{
+        type: String,
+        trim: true
+    }
+})
+
+const bProfileSchema = new mongoose.Schema({
+    beautician_name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    beautician_bio:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    beautician_profilepic:{
+        type:String,
+        required:true,
+    },
+    experience:{
+        type:Number
+    },
+    gallery:{
+        type:String
+    },
+    pricing:{
+        type:String
+    },
+    services_offered:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    working_hours:{
+        type:String,
+    },
+    certifications:{
+        type:String
+    },
+    rating:{
+        type:Number,
+        default:0
+    },
+    contact_info:contactInfoSchema,
+    socials:socialsSchema
+},{timestamps:true})
+
+module.exports=mongoose.model('B_Profile',bProfileSchema)
