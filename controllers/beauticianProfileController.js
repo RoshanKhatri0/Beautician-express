73b7775e62960102d4df626e4e0af3baa15733b6
RoshanKhatri0 <lastkhatri@gmail.com>
2024-03-16 20:40:08 +0545

    apply beautician

--- controllers/beauticianProfileController.js
+++ controllers/beauticianProfileController.js
@@ -1,35 +1,56 @@
-const B_Profile =require('../models/beauticianProfileModel')
+const B_Profile =require('../models/beauticianProfileModel');
+const userModel = require('../models/userModel');
 
 exports.postB_Profile=async(req,res)=>{
-    let contactInfo = {
-        email: req.body.email,
-        phoneNumber: req.body.phoneNumber
-      };
-    
-      let socialsData = {
-        socials_instragram: req.body.socials_instragram,
-        socials_facebook: req.body.socials_facebook,
-        socials_tiktok: req.body.socials_tiktok
-      };
-    let b_profile = new B_Profile({
-        beautician_name: req.body.beautician_name,
-        beautician_bio: req.body.beautician_bio,
-        beautician_profilepic: req.file.path,
-        experience: req.body.experience,
-        gallery: req.body.gallery,
-        pricing: req.body.pricing,
-        services_offered: req.body.services_offered,
-        working_hours: req.body.working_hours,
-        certifications: req.body.certifications,
-        contact_info: contactInfo,
-        socials: socialsData,
+    try{
+        let contactInfo = {
+            email: req.body.email,
+            phoneNumber: req.body.phoneNumber
+          };
         
-    })
-    b_profile = await b_profile.save()
-    if(!b_profile){
-        return res.status(400).json({error:'Something went wrong'})
+          let socialsData = {
+            socials_instragram: req.body.socials_instragram,
+            socials_facebook: req.body.socials_facebook,
+            socials_tiktok: req.body.socials_tiktok
+          };
+        let b_profile = new B_Profile({
+            beautician_name: req.body.beautician_name,
+            beautician_bio: req.body.beautician_bio,
+            beautician_profilepic: req.file.path,
+            experience: req.body.experience,
+            gallery: req.body.gallery,
+            pricing: req.body.pricing,
+            services_offered: req.body.services_offered,
+            working_hours: req.body.working_hours,
+            certifications: req.body.certifications,
+            contact_info: contactInfo,
+            socials: socialsData,
+            
+        })
+        await b_profile.save()
+        const adminUser = await userModel.findOne({role:1})
+        const notification = adminUser.notification
+        notification.push({
+            type:"apply-beautician-request",
+            message: `${b_profile.beautician_name} has applied for a Beautician Account`,
+            data:{
+                b_Id: b_profile._id,
+                name: b_profile.beautician_name,
+                onClickPath:'/admin/beuticians'
+            }
+        })
+        await userModel.findByIdAndUpdate(adminUser._id, {notification})
+        res.status(201).json({
+            success:true,
+            message: "Beautician Account Applied Successfully"
+        })
     }
-    res.send(b_profile)
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            success:false, error, message:"Error while applying for beautician"
+        })
+    }
 }
 
 exports.B_ProfileList=async(req,res)=>{
--- routes/beautificanProfileRoute.js
+++ routes/beautificanProfileRoute.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router=express.Router()
 const { postB_Profile, B_ProfileList, B_ProfileDetail, B_ProfileUpdate, B_ProfileDelete } = require('../controllers/beauticianProfileController');
 const upload = require('../middleware/fileUpload');
+const authMware = require('../middleware/authMware');
 
-router.post('/b_profilepost',upload.single('beautician_profilepic') , postB_Profile )
+router.post('/apply-beautician',upload.single('beautician_profilepic'), authMware , postB_Profile )
 router.get('/b_profilelist',B_ProfileList)
 router.get('/b_profiledetail/:id',B_ProfileDetail)
 router.put('/b_profileupdate/:id',upload.single('beautician_profilepic'),B_ProfileUpdate)
