db1af45839100da88f3bf656b6c253839d0e1b57
RoshanKhatri0 <lastkhatri@gmail.com>
2024-04-03 21:34:11 +0545

    validation added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { postUser, signIn, authController, getAllNotificationController, deleteAllNotification, bookAppointment, userAppointmentList } = require('../controllers/userController')
 const authMware = require('../middleware/authMware')
+const { userValidation, validation, passwordValidation } = require('../validator/validator')
 const router = express.Router()
 
-router.post('/register',postUser)
-router.post('/login',signIn)
+router.post('/register', userValidation , passwordValidation , validation , postUser)
+router.post('/login', signIn)
 router.post('/getUserData',authMware, authController)
 router.post('/get-all-notification',authMware, getAllNotificationController)
 router.post('/delete-all-notification',authMware, deleteAllNotification)
